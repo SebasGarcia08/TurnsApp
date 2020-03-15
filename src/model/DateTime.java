9968de1b173fe271baa55a3469af47ee7e1c7df8
Sebastian García Acosta <segaracos@outlook.com>
2020-03-15 12:56:13 -0500

    Javadoc generated

--- src/model/DateTime.java
+++ src/model/DateTime.java
@@ -29,6 +29,17 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		MONTHS[1] = ( (year % 4) == 0 ) ? 29: 28;
 	}
 	
+	/**
+	 * Fabric method for returning a new object of this class with specified fields
+	 * @param year, int 
+	 * @param month, in
+	 * @param day, int
+	 * @param hours, int
+	 * @param minutes, int
+	 * @param seconds, int
+	 * @return DateTime
+	 * @throws Exception if fields are not valid
+	 */
 	public static DateTime of(int year, int month, int day, int hours, int minutes, int seconds) throws Exception {
 		MONTHS[1] = ( (year % 4) == 0 ) ? 29: 28;
 		if( month == 2 && day > 28 && year % 4 != 0  )
@@ -46,38 +57,10 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		else return new DateTime(year, month, day, minutes, hours, seconds);
 	}
 	
-	public int getHours() {
-		return hours;
-	}
-
-	public void setHours(int hours) {
-		this.hours = hours;
-	}
-
-	public int getMinutes() {
-		return minutes;
-	}
-	
-	public void setMinutes(int minutes) {
-		this.minutes = minutes;
-	}
-	
-	public int getSeconds() {
-		return seconds;
-	}
-	
-	public void setSeconds(int seconds) {
-		this.seconds = seconds;
-	}
-	
-	public String stringDate() {
-		return hours + ":" + minutes + ":" + seconds;
-	}
-	
-	public String stringTime() {
-		return year + "-" +  month + "-" + day;
-	}
-	
+	/**
+	 * Adds the milliseconds passed as parameter to the datetime
+	 * @param millis
+	 */
 	public void plusMillis(long millis) {
 		int newSecs = (int) (millis / 1000);
 		int actualSecs = this.seconds + newSecs;
@@ -89,10 +72,61 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		else { this.seconds = actualSecs; }
 	}
 	
+	/**
+	 * @return the hours
+	 */
+	public int getHours() {
+		return hours;
+	}
+
+	/**
+	 * @param hours the hours to set
+	 */
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+	/**
+	 * @return the minutes
+	 */
+	public int getMinutes() {
+		return minutes;
+	}
+
+	/**
+	 * @param minutes the minutes to set
+	 */
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+	/**
+	 * @return the seconds
+	 */
+	public int getSeconds() {
+		return seconds;
+	}
+
+	/**
+	 * @param seconds the seconds to set
+	 */
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+
+	/**
+	 * Fabric method for returning object with same fields 
+	 * @param dt, the DateTime to be cloned
+	 * @return DateTime, new object with same fields as the one passed by params.
+	 */
 	public static DateTime copyOf( DateTime dt){
 		return new DateTime(dt.year, dt.month, dt.day, dt.hours, dt.minutes, dt.seconds);
 	}
 	
+	/**
+	 * Adds the minutes passed as parameter to the datetime
+	 * @param newMinutes
+	 */
 	public void plusMinutes(int newMinutes) {
 		int actualMins = this.minutes + newMinutes;
 		if(actualMins >= 60) {
@@ -102,6 +136,10 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		}else { this.minutes = actualMins; }
 	}
 	
+	/**
+	 * Adds the hours passed as parameter to the datetime
+	 * @param newHours
+	 */
 	public void plusHours(int newHours) {
 		int actualHours = this.hours + newHours;
 		if(actualHours >= 24) {
@@ -111,6 +149,10 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		}else { this.hours = actualHours; }
 	}
 	
+	/**
+	 * Adds the days passed as parameter to the datetime
+	 * @param newDays
+	 */
 	public void plusDays(int newDays) {
 		int actualDays = this.day + newDays;
 		if( actualDays >= MONTHS[month - 1] ) {
@@ -120,6 +162,10 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		} else { this.day = actualDays; }
 	}
 	
+	/**
+	 * Adds the motnhs passed as parameter to the datetime
+	 * @param newMonths
+	 */
 	public void plusMonths(int newMonths) {
 		int actualMonth = this.month + newMonths;
 		if(actualMonth >= 12) {
@@ -129,6 +175,11 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		}else {this.month = actualMonth; }
 	}
 	
+	/**
+	 * Adds the year passed as parameter to the datetime
+	 * <b>pre: </b> newYears must be les than 1000
+	 * @param newYears
+	 */
 	public void plusYears(int newYears) {
 		int actualYear = this.year + newYears;
 		this.year = actualYear;
@@ -137,6 +188,10 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		} else { this.year = actualYear; }*/
 	}
 	
+	/**
+	 * Fabric method for returning the current datetime
+	 * @return
+	 */
 	public static DateTime now() {
 		return new DateTime(LocalDateTime.now().getYear(), 
 				LocalDateTime.now().getMonthValue(), 
@@ -146,49 +201,39 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 				LocalDateTime.now().getSecond());
 	}
 	
+	/**
+	 * Converts datetime to LocalDateTime object
+	 * @return LocalDateTime
+	 */
 	public LocalDateTime toLocalDateTime() {
 		return LocalDateTime.of(year, month, day, hours, minutes, seconds);
 	}
 	
+	/**
+	 * Accelerates the time in order to make this datetime at midnight
+	 * @return DateTime, this datetime in midnight
+	 */
 	public DateTime asMidnight() {
 		return new DateTime(year, month, day, 23, 59, 59);
 	}
 	
+	/**
+	 * Tells if two this datetime is between other tow
+	 * @param d1 DateTime
+	 * @param d2 DateTime
+	 * @return boolean, if this datetime is between d1 and d2
+	 */
 	public boolean isBetween(DateTime d1, DateTime d2) {
 		return ( this.isAfter(d1) && this.isBefore(d2) );
 	}
 	
-	public DateTime fiveMinutesBeforeMidNight() {
-		return new DateTime(this.year, this.month, this.day, 23, 55, 00);
-	}
-	
+	/**
+	 * Converts minutes to milliscoends
+	 * @param minutes, flaot
+	 * @return millisconds of minutes
+	 */
 	public static long minutes2Millis(float minutes) {
 		return (long) ((minutes * 60) * 1000);
-	}
-	
-	public int getYear() {
-		return year;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-		MONTHS[1] = ( year % 4 == 0 ) ? 29: 28;
-	}
-
-	public int getMonth() {
-		return month;
-	}
-
-	public void setMonth(int month) {
-		this.month = month;
-	}
-
-	public int getDay() {
-		return day;
-	}
-
-	public void setDay(int day) {
-		this.day = day;
 	}
 
 	@Override
@@ -196,14 +241,66 @@ public class DateTime implements Serializable, Comparable<DateTime>{
 		return year + "-" +  month + "-" + day + " " +  hours + ":" + minutes + ":" + seconds;
 	}
 	
+	/**
+	 * Tells if this datetime is before the one passed by param
+	 * @param dt
+	 * @return
+	 */
 	public boolean isBefore( DateTime dt) {
 		return ( this.compareTo(dt) == -1 );
 	}
 	
+	/**
+	 * Tells if this datetime is after the one passed by param
+	 * @param dt
+	 * @return
+	 */
 	public boolean isAfter(DateTime dt) {
 		return ( this.compareTo(dt) == 1 );		
 	}
 	
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/**
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/**
+	 * @param month the month to set
+	 */
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	/**
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
+
+	/**
+	 * @param day the day to set
+	 */
+	public void setDay(int day) {
+		this.day = day;
+	}
+
 	@Override
 	public int compareTo(DateTime o) {
 		int result;
